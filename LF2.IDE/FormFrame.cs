ec16c7e0722979fa7008258f02959e0ba1de099e
Ahmet Sait <git@ahmetsait.com>
2015-06-24 17:10:13 +0300

    StringBuilder instead of string aritmetic

--- LF2.IDE/FormFrame.cs
+++ LF2.IDE/FormFrame.cs
@@ -38,52 +38,51 @@ namespace LF2.IDE
 		private void Insert(object sender, EventArgs e)
 		{
 			if (mainForm.ActiveDocument != null)
-				mainForm.ActiveDocument.Scintilla.Selection.Text = richTextBox.Text;
+				mainForm.ActiveDocument.Scintilla.Selection.Text = mainForm.ActiveDocument.Scintilla.EndOfLine.Mode == ScintillaNET.EndOfLineMode.LF ? richTextBox.Text.Replace("\r", ""): richTextBox.Text;
 		}
 
 		private void Generate(object sender, EventArgs e)
 		{
 			int nextStep; int.TryParse(next.Text.Trim(), out nextStep);
-			string str = "<frame> " + numericUpDown_frameIndex.Value + " " + ComboBox_caption.Text.Trim() + "\n   " + "pic: " + numericUoDown_imageIndex.Value + "  state: " + state.Text.Trim() + "  wait: " + wait.Text.Trim() + "  next: " + next.Text.Trim() + "  dvx: " + dvx.Text.Trim() + "  dvy: " + dvy.Text.Trim() + (dvz.Text != "" ? "  dvz: " + dvz.Text.Trim() : "") + "  centerx: " + centerx.Text.Trim() + "  centery: " + centery.Text.Trim();
-			str += hit_a.Text.Trim() == "" ? "" : "  hit_a: " + hit_a.Text.Trim();
-			str += hit_d.Text.Trim() == "" ? "" : "  hit_d: " + hit_d.Text.Trim();
-			str += hit_j.Text.Trim() == "" ? "" : "  hit_j: " + hit_j.Text.Trim();
-			str += hit_Fa.Text.Trim() == "" ? "" : "  hit_Fa: " + hit_Fa.Text.Trim();
-			str += hit_Ua.Text.Trim() == "" ? "" : "  hit_Ua: " + hit_Ua.Text.Trim();
-			str += hit_Da.Text.Trim() == "" ? "" : "  hit_Da: " + hit_Da.Text.Trim();
-			str += hit_Fj.Text.Trim() == "" ? "" : "  hit_Fj: " + hit_Fj.Text.Trim();
-			str += hit_Uj.Text.Trim() == "" ? "" : "  hit_Uj: " + hit_Uj.Text.Trim();
-			str += hit_Dj.Text.Trim() == "" ? "" : "  hit_Dj: " + hit_Dj.Text.Trim();
-			str += hit_ja.Text.Trim() == "" ? "" : "  hit_ja: " + hit_ja.Text.Trim();
-			str += sound.Text.Trim() == "" ? "" : "\n  sound: " + sound.Text.Trim();
-			if (checkBox_AddTags.Checked && mainForm.formTag.richTextBox.Text != "") str += "\n" + mainForm.formTag.richTextBox.Text;
-			str += "\n<frame_end>";
+			StringBuilder str = new StringBuilder().AppendFormat("<frame> {0} {1}\n   pic: {2}  state: {3}  wait: {4}  next: {5}  dvx: {6}  dvy: {7}{8}  centerx: {9}  centery: {10}", numericUpDown_frameIndex.Value, ComboBox_caption.Text.Trim(), numericUoDown_imageIndex.Value, state.Text.Trim(), wait.Text.Trim(), next.Text.Trim(), dvx.Text.Trim(), dvy.Text.Trim(), (dvz.Text != "" ? "  dvz: " + dvz.Text.Trim() : ""), centerx.Text.Trim(), centery.Text.Trim());
+			str.Append(hit_a.Text.Trim() == "" ? "" : "  hit_a: " + hit_a.Text.Trim());
+			str.Append(hit_d.Text.Trim() == "" ? "" : "  hit_d: " + hit_d.Text.Trim());
+			str.Append(hit_j.Text.Trim() == "" ? "" : "  hit_j: " + hit_j.Text.Trim());
+			str.Append(hit_Fa.Text.Trim() == "" ? "" : "  hit_Fa: " + hit_Fa.Text.Trim());
+			str.Append(hit_Ua.Text.Trim() == "" ? "" : "  hit_Ua: " + hit_Ua.Text.Trim());
+			str.Append(hit_Da.Text.Trim() == "" ? "" : "  hit_Da: " + hit_Da.Text.Trim());
+			str.Append(hit_Fj.Text.Trim() == "" ? "" : "  hit_Fj: " + hit_Fj.Text.Trim());
+			str.Append(hit_Uj.Text.Trim() == "" ? "" : "  hit_Uj: " + hit_Uj.Text.Trim());
+			str.Append(hit_Dj.Text.Trim() == "" ? "" : "  hit_Dj: " + hit_Dj.Text.Trim());
+			str.Append(hit_ja.Text.Trim() == "" ? "" : "  hit_ja: " + hit_ja.Text.Trim());
+			str.Append(sound.Text.Trim() == "" ? "" : "\n  sound: " + sound.Text.Trim());
+			if (checkBox_AddTags.Checked && mainForm.formTag.richTextBox.Text != "")
+				str.Append("\n" + mainForm.formTag.richTextBox.Text);
+			str.Append("\n<frame_end>");
 
-			if (frameCount.Value > 1)
+			for (int i = 0; i < frameCount.Value; i++)
 			{
-				for (int i = 1; i < frameCount.Value; i++)
-				{
-					str += "\n\n<frame> " + (numericUpDown_frameIndex.Value + i) + " " + ComboBox_caption.Text.Trim() + "\n   " + "pic: " + (numericUoDown_imageIndex.Value + i) + "  state: " + state.Text.Trim() + "  wait: " + wait.Text.Trim() + "  next: " + (i == frameCount.Value - 1 ? nextOfLastFrame.Text : (nextStep + i).ToString()) + "  dvx: " + dvx.Text.Trim() + "  dvy: " + dvy.Text.Trim() + (dvz.Text != "" ? "  dvz: " + dvz.Text.Trim() : "") + "  centerx: " + centerx.Text.Trim() + "  centery: " + centery.Text.Trim();
-					str += hit_a.Text.Trim() == "" ? "" : "  hit_a: " + hit_a.Text.Trim();
-					str += hit_d.Text.Trim() == "" ? "" : "  hit_d: " + hit_d.Text.Trim();
-					str += hit_j.Text.Trim() == "" ? "" : "  hit_j: " + hit_j.Text.Trim();
-					str += hit_Fa.Text.Trim() == "" ? "" : "  hit_Fa: " + hit_Fa.Text.Trim();
-					str += hit_Ua.Text.Trim() == "" ? "" : "  hit_Ua: " + hit_Ua.Text.Trim();
-					str += hit_Da.Text.Trim() == "" ? "" : "  hit_Da: " + hit_Da.Text.Trim();
-					str += hit_Fj.Text.Trim() == "" ? "" : "  hit_Fj: " + hit_Fj.Text.Trim();
-					str += hit_Uj.Text.Trim() == "" ? "" : "  hit_Uj: " + hit_Uj.Text.Trim();
-					str += hit_Dj.Text.Trim() == "" ? "" : "  hit_Dj: " + hit_Dj.Text.Trim();
-					str += hit_ja.Text.Trim() == "" ? "" : "  hit_ja: " + hit_ja.Text.Trim();
-					str += sound.Text.Trim() == "" ? "" : "\n  sound: " + sound.Text.Trim();
-					if (checkBox_AddTags.Checked && mainForm.formTag.richTextBox.Text != "") str += "\n" + mainForm.formTag.richTextBox.Text;
-					str += "\n<frame_end>";
-				}
+				str = new StringBuilder().AppendFormat("<frame> {0} {1}\n   pic: {2}  state: {3}  wait: {4}  next: {5}  dvx: {6}  dvy: {7}{8}  centerx: {9}  centery: {10}", numericUpDown_frameIndex.Value, ComboBox_caption.Text.Trim(), numericUoDown_imageIndex.Value, state.Text.Trim(), wait.Text.Trim(), next.Text.Trim(), dvx.Text.Trim(), dvy.Text.Trim(), (dvz.Text != "" ? "  dvz: " + dvz.Text.Trim() : ""), centerx.Text.Trim(), centery.Text.Trim());
+				str.Append(hit_a.Text.Trim() == "" ? "" : "  hit_a: " + hit_a.Text.Trim());
+				str.Append(hit_d.Text.Trim() == "" ? "" : "  hit_d: " + hit_d.Text.Trim());
+				str.Append(hit_j.Text.Trim() == "" ? "" : "  hit_j: " + hit_j.Text.Trim());
+				str.Append(hit_Fa.Text.Trim() == "" ? "" : "  hit_Fa: " + hit_Fa.Text.Trim());
+				str.Append(hit_Ua.Text.Trim() == "" ? "" : "  hit_Ua: " + hit_Ua.Text.Trim());
+				str.Append(hit_Da.Text.Trim() == "" ? "" : "  hit_Da: " + hit_Da.Text.Trim());
+				str.Append(hit_Fj.Text.Trim() == "" ? "" : "  hit_Fj: " + hit_Fj.Text.Trim());
+				str.Append(hit_Uj.Text.Trim() == "" ? "" : "  hit_Uj: " + hit_Uj.Text.Trim());
+				str.Append(hit_Dj.Text.Trim() == "" ? "" : "  hit_Dj: " + hit_Dj.Text.Trim());
+				str.Append(hit_ja.Text.Trim() == "" ? "" : "  hit_ja: " + hit_ja.Text.Trim());
+				str.Append(sound.Text.Trim() == "" ? "" : "\n  sound: " + sound.Text.Trim());
+				if (checkBox_AddTags.Checked && mainForm.formTag.richTextBox.Text != "")
+					str.Append("\n" + mainForm.formTag.richTextBox.Text);
+				str.Append("\n<frame_end>");
 			}
 
 			if (string.IsNullOrEmpty(richTextBox.Text))
-				richTextBox.Text = str;
+				richTextBox.Text = str.ToString();
 			else
-				richTextBox.Text += "\n\n" + str;
+				richTextBox.Text += "\n\n" + str.ToString();
 
 			richTextBox.SelectionStart = richTextBox.Text.Length;
 			richTextBox.ScrollToCaret();
@@ -120,7 +119,7 @@ namespace LF2.IDE
 				mainForm.ActiveDocument.Scintilla.Selection.Text = (mainForm.ActiveDocument.Scintilla.EndOfLine.Mode == ScintillaNET.EndOfLineMode.LF ? "\n" : "\r\n");
 		}
 
-		void FormVisualFormClosing(object sender, FormClosingEventArgs e)
+		void FormFrame_Closing(object sender, FormClosingEventArgs e)
 		{
 
 		}
