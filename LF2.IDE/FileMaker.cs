aa54145db47121e56d7a6a0b8090b0c144e60f89
Ahmet Sait <git@ahmetsait.com>
2015-06-25 16:25:58 +0300

    Made SpriteSheet

--- LF2.IDE/FileMaker.cs
+++ LF2.IDE/FileMaker.cs
@@ -3,31 +3,37 @@ using System.Drawing;
 
 namespace LF2.IDE
 {
-	public class FileMaker
+	public class SpriteSheet
 	{
-		public FileMaker(int startIndex, int endIndex, string name, Bitmap image, int w, int h, int row, int col)
-			: base()
+		public SpriteSheet() { }
+
+		public SpriteSheet(int startIndex, int endIndex, string file, Bitmap sprite, int w, int h, int row, int col)
 		{
-			this.name = name;
+			this.file = file;
 			this.startIndex = startIndex;
 			this.endIndex = endIndex;
-			this.image = image;
+			this.sprite = sprite;
 			this.w = w;
 			this.h = h;
 			this.row = row;
 			this.col = col;
 		}
 
-		public string name;
-		public Bitmap image;
-		public int w, h, row, col;
-		public int startIndex, endIndex;
+		public Bitmap sprite;
+
+		public string file;
+		public int w;
+		public int h;
+		public int row;
+		public int col;
+		public int startIndex;
+		public int endIndex;
 
 		public const string regexPattern = @"file\((\d*)-(\d*)\): *(.*) *w: *(\d*) *h: *(\d*) *row: *(\d*) *col: *(\d*)";
 
 		public override string ToString()
 		{
-			return ("file(" + startIndex + "-" + endIndex + ")" + ": " + name + "  w: " + w + "  h: " + h + "  row: " + row + "  col: " + col);
+			return ("file(" + startIndex + "-" + endIndex + ")" + ": " + file + "  w: " + w + "  h: " + h + "  row: " + row + "  col: " + col);
 		}
 	}
 }
