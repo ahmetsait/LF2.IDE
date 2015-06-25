7c1ab04188a3bdfb43a73d20e9009027829b29ae
Ahmet Sait <git@ahmetsait.com>
2015-06-25 16:34:21 +0300

    Nothing useful happened

--- LF2.IDE/SpriteSheet.cs
+++ LF2.IDE/SpriteSheet.cs
@@ -1,71 +1,107 @@
-﻿using System;
+﻿using ProtoBuf;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LF2.IDE
+[ProtoContract]
+class SpriteSheet2
 {
-	public class SpriteSheet
+	Bitmap sprite;
+	Size size;
+	Size space;
+	// Watch out! LF2 use these for each other (row <=> col) (Used normally in code)
+	int row;
+	int col;
+	int indexStart;
+	int indexEnd;
+
+	public Bitmap Sprite
 	{
-		Bitmap sprite;
-		Size size;
-		Size space;
-		// Watch out! LF2 use these for each other (row <=> col) (Used normally in code)
-		int row, col;
+		get { return sprite; }
+	}
 
-		public Bitmap Sprite
-		{
-			get { return sprite; }
-		}
+	[ProtoMember(1)]
+	public Size Size
+	{
+		get { return size; }
+	}
 
-		public Size Size
-		{
-			get { return size; }
-		}
+	[ProtoMember(2)]
+	public Size Space
+	{
+		get { return space; }
+	}
 
-		public Size Space
-		{
-			get { return space; }
-		}
+	[ProtoMember(3)]
+	public int Row
+	{
+		get { return row; }
+		set { row = value; }
+	}
 
-		public SpriteSheet(Bitmap sprite, Size size, Size space)
-		{
-			this.sprite = sprite;
-			this.size = size;
-			this.space = space;
-			row = sprite.Width / (size.Width + space.Width);
-			col = sprite.Height / (size.Height + space.Height);
-		}
+	[ProtoMember(4)]
+	public int Col
+	{
+		get { return col; }
+		set { col = value; }
+	}
 
-		public Bitmap this[int index]
-		{
-			get
-			{
-				return this[index % col, index / row];
-			}
-		}
+	[ProtoMember(5)]
+	public int IndexStart
+	{
+		get { return indexStart; }
+		set { indexStart = value; }
+	}
 
-		public Bitmap this[int x, int y]
-		{
-			get
-			{
-				Bitmap result = new Bitmap(size.Width, size.Height);
-				using (Graphics gr = Graphics.FromImage(result))
-					gr.DrawImage(sprite, new Rectangle(0, 0, size.Width, size.Height), new Rectangle(x * (size.Width + space.Width), y * (size.Height + space.Height), size.Width, size.Height), GraphicsUnit.Pixel);
-				return result;
-			}
-		}
+	[ProtoMember(6)]
+	public int IndexEnd
+	{
+		get { return indexEnd; }
+		set { indexEnd = value; }
+	}
 
-		public Rectangle GetArea(int index)
-		{
-			return GetArea(index % col, index / row);
-		}
+	public SpriteSheet2() { }
 
-		public Rectangle GetArea(int x, int y)
+	public SpriteSheet2(Bitmap sprite, Size size, Size space, int indexStart, int indexEnd)
+	{
+		this.sprite = sprite;
+		this.size = size;
+		this.space = space;
+		row = sprite.Width / (size.Width + space.Width);
+		col = sprite.Height / (size.Height + space.Height);
+		this.indexStart = indexStart;
+		this.indexEnd = indexEnd;
+	}
+
+	public Bitmap this[int imageIndex]
+	{
+		get
 		{
-			return new Rectangle(x * (size.Width + space.Width), y * (size.Height + space.Height), size.Width, size.Height);
+			return this[(imageIndex - indexStart) % col, (imageIndex - indexStart) / row];
 		}
+	}
+
+	public Bitmap this[int x, int y]
+	{
+		get
+		{
+			Bitmap result = new Bitmap(size.Width, size.Height);
+			using (Graphics gr = Graphics.FromImage(result))
+				gr.DrawImage(sprite, new Rectangle(0, 0, size.Width, size.Height), new Rectangle(x * (size.Width + space.Width), y * (size.Height + space.Height), size.Width, size.Height), GraphicsUnit.Pixel);
+			return result;
+		}
+	}
+
+	public Rectangle GetArea(int imageIndex)
+	{
+		return GetArea((imageIndex - indexStart) % col, (imageIndex - indexStart) / row);
+	}
+
+	public Rectangle GetArea(int x, int y)
+	{
+		return new Rectangle(x * (size.Width + space.Width), y * (size.Height + space.Height), size.Width, size.Height);
 	}
 }
