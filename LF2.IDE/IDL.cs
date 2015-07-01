89bd1175ff7d6f5f701555ff951d0c3f78f2afa6
Ahmet Sait <git@ahmetsait.com>
2015-07-01 09:09:23 +0300

    InstantDataLoader place holder

---
+++ LF2.IDE/IDL.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LF2.IDE
+{
+	public class IDL
+	{
+		[DllImport("InstantDataModifier.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int InstantDataLoader(string data, int procId, int objId, int datType);
+	}
+}
