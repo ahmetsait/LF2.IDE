335d300819eac1b5ff9b488924ecd1118265d9c6
Ahmet Sait <git@ahmetsait.com>
2015-07-03 15:44:54 +0300

    Improved EndOfLine handling

--- LF2.IDE/FormFrame.cs
+++ LF2.IDE/FormFrame.cs
@@ -39,7 +39,8 @@ namespace LF2.IDE
 		private void Insert(object sender, EventArgs e)
 		{
 			if (mainForm.ActiveDocument != null)
-				mainForm.ActiveDocument.Scintilla.Selection.Text = mainForm.ActiveDocument.Scintilla.EndOfLine.Mode == ScintillaNET.EndOfLineMode.LF ? richTextBox.Text.Replace("\r", string.Empty) : richTextBox.Text;
+				mainForm.ActiveDocument.Scintilla.Selection.Text = richTextBox.Text;
+			mainForm.ActiveDocument.Scintilla.EndOfLine.ConvertAllLines(mainForm.ActiveDocument.Scintilla.EndOfLine.Mode);
 		}
 
 		private void Generate(object sender, EventArgs e)
@@ -83,7 +84,7 @@ namespace LF2.IDE
 			if (string.IsNullOrEmpty(richTextBox.Text))
 				richTextBox.Text = str.ToString();
 			else
-				richTextBox.Text += "\r\n\n" + str.ToString();
+				richTextBox.Text += "\r\n\r\n" + str.ToString();
 
 			richTextBox.SelectionStart = richTextBox.Text.Length;
 			richTextBox.ScrollToCaret();
@@ -117,7 +118,7 @@ namespace LF2.IDE
 		void NewLine(object sender, EventArgs e)
 		{
 			if (mainForm.ActiveDocument != null)
-				mainForm.ActiveDocument.Scintilla.Selection.Text = (mainForm.ActiveDocument.Scintilla.EndOfLine.Mode == ScintillaNET.EndOfLineMode.LF ? "\n" : System.Environment.NewLine);
+				mainForm.ActiveDocument.Scintilla.Selection.Text = mainForm.ActiveDocument.Scintilla.EndOfLine.EolString;
 		}
 
 		void FormFrame_Closing(object sender, FormClosingEventArgs e)
