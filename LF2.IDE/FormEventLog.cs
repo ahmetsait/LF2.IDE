dc8c8c147955a68ac578a5bfbae2f71280543980
Ahmet Sait <git@ahmetsait.com>
2015-07-28 19:06:19 +0300

    Changed RichTextBox to Scintilla and fixed async log bug

--- LF2.IDE/FormEventLog.cs
+++ LF2.IDE/FormEventLog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace LF2.IDE
@@ -17,7 +18,7 @@ namespace LF2.IDE
 			string error = ex.ToString();
 
 			if (this.InvokeRequired)
-				this.Invoke((Action)(() => MessageBox.Show(error, "[Async] " + title, MessageBoxButtons.OK, MessageBoxIcon.Error)));
+				this.BeginInvoke((Action)(() => MessageBox.Show(error, "[Async] " + title, MessageBoxButtons.OK, MessageBoxIcon.Error)));
 			else
 				MessageBox.Show(error, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
 			try
@@ -27,56 +28,131 @@ namespace LF2.IDE
 			catch { }
 		}
 
-		public void Log(string logMessage, string logTitle, bool stepCaret)
+		public void Log(string logMessage, string logTitle, bool stepCaret = true)
 		{
 			Action act = (Action)(() =>
 				{
-					Log(">>\t" + logTitle, false);
-					int caret = richTextBox.Text.Length;
-					richTextBox.AppendText(Environment.NewLine + logMessage);
+					int caret = scintilla.Lines.Count - 1;
+					Log(logTitle, false);
+					scintilla.AppendText(scintilla.EndOfLine.EolString + logMessage);
 					StepCaret(stepCaret);
+					StepCaret(caret, stepCaret);
+					scintilla.Lines.Current.Next.AddMarker(scintilla.Markers.Folder);
 				});
 
-			if (this.InvokeRequired)
-				this.Invoke(act);
-			else
-				act();
+			try
+			{
+				if (this.InvokeRequired)
+					this.BeginInvoke(act);
+				else
+					act();
+			}
+			catch (AccessViolationException) { }
 		}
 
-		public void Log(string logMessage, bool stepCaret)
+		public void Log(string logMessage, bool stepCaret = true)
 		{
-			Action act = (Action)(() =>
+			Action<string> act = (Action<string>)((string msg) =>
 				{
-					if (!string.IsNullOrEmpty(richTextBox.Text))
-						richTextBox.AppendText(System.Environment.NewLine + System.Environment.NewLine);
-					richTextBox.AppendText(logMessage);
+					if (!string.IsNullOrEmpty(scintilla.Text))
+						scintilla.AppendText(scintilla.EndOfLine.EolString);
+					scintilla.AppendText(msg);
 					StepCaret(stepCaret);
 				});
 
-			if (richTextBox.InvokeRequired)
-				richTextBox.Invoke(act);
-			else
-				act();
+			try
+			{
+				if (scintilla.InvokeRequired)
+					scintilla.BeginInvoke(act, logMessage);
+				else
+					act(logMessage);
+			}
+			catch (AccessViolationException) { }
 		}
 
-		private void StepCaret(bool condition)
+		private void StepCaret(bool condition = true)
 		{
 			if (condition)
 			{
-				richTextBox.Select(richTextBox.Text.Length, 0);
-				richTextBox.ScrollToCaret();
-				richTextBox.Update();
+				scintilla.Caret.LineNumber = scintilla.Lines.Count - 1;
+				scintilla.Caret.EnsureVisible();
+				scintilla.Update();
+				scintilla.Refresh();
+			}
+		}
+
+		private void StepCaret(int caretLine, bool condition = true)
+		{
+			if (condition)
+			{
+				scintilla.Caret.LineNumber = caretLine;
+				scintilla.Caret.EnsureVisible();
+				scintilla.Update();
+				scintilla.Refresh();
 			}
 		}
 
 		public void Clean()
 		{
-			richTextBox.Clear();
+			scintilla.IsReadOnly = false;
+			scintilla.ResetText();
+			scintilla.Update();
+			scintilla.Refresh();
+			scintilla.IsReadOnly = true;
 		}
 
-		void RichTextBox_LinkClicked(object sender, LinkClickedEventArgs e)
+		void scintilla_LinkClicked(object sender, LinkClickedEventArgs e)
 		{
 			Process.Start(e.LinkText);
+		}
+
+		void ScintillaZoomChanged(object sender, EventArgs e)
+		{
+			SetMarginAuto();
+		}
+
+		void ScintillaTextChanged(object sender, EventArgs e)
+		{
+			SetMarginAuto();
+		}
+
+		public void SetMarginAuto()
+		{
+			int last_measure_lines = -1;
+			int lines = scintilla.Lines.Count;
+			if (lines != last_measure_lines)
+			{
+				last_measure_lines = lines;
+				scintilla.Margins[0].Width = TextRenderer.MeasureText(lines.ToString(), scintilla.Font).Width + scintilla.Zoom * 3;
+			}
+		}
+
+		private void scintilla_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			string link = scintilla.Lines.Current.Text.Trim('\r', '\n', ' ', '\t');
+			if (link.StartsWith("http://"))
+				scintilla_LinkClicked(scintilla, new LinkClickedEventArgs(link));
+		}
+
+		private void scintilla_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (!(e.Control && e.KeyCode == Keys.C))
+				e.SuppressKeyPress = e.Handled = true;
+		}
+
+		private void contextMenuStrip_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			//e.Cancel = true;
+		}
+
+		private void copySelectedToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			scintilla.Clipboard.Copy(true);
+		}
+
+		private void copyAllToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			scintilla.Clipboard.Copy(0, scintilla.TextLength);
 		}
 	}
 }
