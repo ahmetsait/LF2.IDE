cb737ba2302334042c968fd6e86b0e55ee5f9dec
Ahmet Sait <git@ahmetsait.com>
2015-08-06 22:54:36 +0300

    Frame Merge function completely done

--- LF2.IDE/FormFrame.Designer.cs
+++ LF2.IDE/FormFrame.Designer.cs
@@ -1292,7 +1292,7 @@
 			// FormFrame
 			// 
 			this.AcceptButton = this.button_Generate;
-			this.AutoHidePortion = 300D;
+			this.AutoHidePortion = 350D;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(334, 526);
