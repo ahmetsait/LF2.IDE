d46efe3bd1dae39ca2b4ea00b114c98683b36e3e
Ahmet Sait <git@ahmetsait.com>
2016-01-05 16:25:16 +0200

    Fix: Sync happens even when focus changes

--- LF2.IDE/FormDesign.cs
+++ LF2.IDE/FormDesign.cs
@@ -1213,7 +1213,7 @@ namespace LF2.IDE
 		{
 			try
 			{
-				if (theDoc == null || theDoc.DocumentType != DocumentType.ObjectData || !Settings.Current.syncDesign)
+				if (theDoc == null || theDoc.DocumentType != DocumentType.ObjectData || !Settings.Current.syncDesign || theDoc.syncing)
 					return false;
 				theDoc.syncing = true;
 				int fs = theDoc.Scintilla.Text.LastIndexOf("<frame>", theDoc.Scintilla.Lines.Current.EndPosition);
