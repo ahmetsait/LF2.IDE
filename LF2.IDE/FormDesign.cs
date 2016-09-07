1df0968c276df478311370544c233fad460583e5
Ahmet Sait <git@ahmetsait.com>
2016-09-07 04:52:04 +0300

    Fix image file loading: open detached

--- LF2.IDE/FormDesign.cs
+++ LF2.IDE/FormDesign.cs
@@ -788,16 +788,23 @@ namespace LF2.IDE
 			{
 				if (backgroundWorker_CreateOpointCache.CancellationPending) return fileList;
 				string path = lfDir + "\\" + matches[i].Groups[3].Value.Trim();
-				Bitmap img = (Bitmap)Image.FromFile(path);
-				img.Tag = Path.GetFileName(path);
-				int si = int.Parse(matches[i].Groups[1].Value),
-					ei = int.Parse(matches[i].Groups[2].Value),
-					w = int.Parse(matches[i].Groups[4].Value),
-					h = int.Parse(matches[i].Groups[5].Value),
-					r = int.Parse(matches[i].Groups[6].Value),
-					c = int.Parse(matches[i].Groups[7].Value);
-				SpriteSheet fm = new SpriteSheet(si, ei, img.Tag as string, img, w, h, c, r);
-				fileList.Add(fm);
+				using (Bitmap temp = (Bitmap)Image.FromFile(path))
+				{
+					Bitmap img = new Bitmap(temp.Width, temp.Height, PixelFormat.Format32bppRgb);
+					using (Graphics gr = Graphics.FromImage(img))
+					{
+						gr.DrawImage(temp, new Rectangle(0, 0, img.Width, img.Height), new Rectangle(0, 0, temp.Width, temp.Height), GraphicsUnit.Pixel);
+					}
+					img.Tag = Path.GetFileName(path);
+					int si = int.Parse(matches[i].Groups[1].Value),
+						ei = int.Parse(matches[i].Groups[2].Value),
+						w = int.Parse(matches[i].Groups[4].Value),
+						h = int.Parse(matches[i].Groups[5].Value),
+						r = int.Parse(matches[i].Groups[6].Value),
+						c = int.Parse(matches[i].Groups[7].Value);
+					SpriteSheet fm = new SpriteSheet(si, ei, img.Tag as string, img, w, h, c, r);
+					fileList.Add(fm);
+				}
 			}
 			return fileList;
 		}
