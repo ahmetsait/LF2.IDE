f8ef1222046c5cf38b6d997b9786c0a04c3b5c09
Ahmet Sait <git@ahmetsait.com>
2018-06-11 13:20:58 +0300

    Proper use of bitmap cloning functionality

--- LF2.IDE/HelperTools.cs
+++ LF2.IDE/HelperTools.cs
@@ -7,6 +7,8 @@ using System.Windows.Forms;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace LF2.IDE
 {
@@ -17,11 +19,25 @@ namespace LF2.IDE
 			Bitmap bmp;
 			using (Bitmap img = new Bitmap(bmpFile))
 			{
-				bmp = new Bitmap(img.Width, img.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+				bmp = new Bitmap(img.Width, img.Height, PixelFormat.Format32bppRgb);
 				using (Graphics g = Graphics.FromImage(bmp))
 					g.DrawImage(img, 0, 0, img.Width, img.Height);
 			}
 			return bmp;
+		}
+
+		public static Bitmap GetDetachedBitmap(string bmpFile)
+		{
+			Bitmap img;
+			using (MemoryStream ms = new MemoryStream())
+			{
+				using (Bitmap bmp = new Bitmap(bmpFile))
+				{
+					bmp.Save(ms, ImageFormat.Bmp);
+				}
+				img = new Bitmap(ms);
+			}
+			return img;
 		}
 
 		public static void SuspendProcess(int procId, bool suspend)
